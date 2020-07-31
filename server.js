77933dd882e69ade601733a83abb1746f227952d
Nick Kalomoiris <nick.kalomoiris17@gmail.com>
2020-08-01 02:36:58 +0300

    Init

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const env = require('dotenv');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
+
+//setting views and layouts
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+
+//Middleware
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false}));
+app.use(expressLayouts);
+app.use(express.static('/public'));
+
+//Routes
+
+//Implement Routes
+
+//Connect Database
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Connect to port ${port}`);
+});
